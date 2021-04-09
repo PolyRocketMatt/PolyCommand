0592639d3e6df60c28220630b7110e0f50bb20ad
Matthias <rocketpencil.studios@gmail.com>
2021-04-09 22:38:49 +0200

    Build 1.4

--- src/main/java/com/github/polyrocketmatt/polycommand/Utils.java
+++ src/main/java/com/github/polyrocketmatt/polycommand/Utils.java
@@ -5,7 +5,28 @@ import org.bukkit.ChatColor;
 public class Utils {
 
     public static String translate(String message) {
-        return ChatColor.translateAlternateColorCodes("§".toCharArray()[0], message);
+        return message.replace("&4", ChatColor.DARK_RED + "")
+                .replace("&c", ChatColor.RED + "")
+                .replace("&6", ChatColor.GOLD + "")
+                .replace("&e", ChatColor.YELLOW + "")
+                .replace("&2", ChatColor.DARK_GREEN + "")
+                .replace("&a", ChatColor.GREEN + "")
+                .replace("&b", ChatColor.AQUA + "")
+                .replace("&3", ChatColor.DARK_AQUA + "")
+                .replace("&1", ChatColor.DARK_BLUE + "")
+                .replace("&9", ChatColor.BLUE + "")
+                .replace("&d", ChatColor.LIGHT_PURPLE + "")
+                .replace("&5", ChatColor.DARK_PURPLE + "")
+                .replace("&f", ChatColor.WHITE + "")
+                .replace("&7", ChatColor.GRAY + "")
+                .replace("&8", ChatColor.DARK_GRAY + "")
+                .replace("&0", ChatColor.BLACK + "")
+                .replace("&k", ChatColor.MAGIC + "")
+                .replace("&l", ChatColor.BOLD + "")
+                .replace("&m", ChatColor.STRIKETHROUGH + "")
+                .replace("&n", ChatColor.UNDERLINE + "")
+                .replace("&o", ChatColor.ITALIC + "")
+                .replace("&r", ChatColor.RESET + "");
     }
 
 }
